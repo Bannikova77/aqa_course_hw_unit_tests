3da18ab346dc28981a126e5c1ebc2a439a05e9d8
Sviatlana Bannikova <atevs2789@gmail.com>
2025-08-12 22:00:03 -0500

    Update homework-solutions/js/hw-6/task-1.mjs
    Co-authored-by: anatoly-karpovich <84677425+anatoly-karpovich@users.noreply.github.com>

--- homework-solutions/js/hw-6/task-1.mjs
+++ homework-solutions/js/hw-6/task-1.mjs
@@ -11,11 +11,8 @@ const numbers = [1,2,3,4,5,6,7,8,9,10];
 const forOf = [];
 
 for (const num of numbers) {
-  if (num % 2 === 0) {
-    forOf.push(num ** 2);
-  } else {
-    forOf.push(num ** 3);
-  }
+    forOf.push(num % 2 === 0 ? num ** 2 : num ** 3);
+
 }
 
 /*
